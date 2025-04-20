52c86436a6199f910643e57da0135239d21477d1
idozii <tue.nguyenpc0401@hcmut.edu.vn>
2025-04-21 00:08:46 +0700

    commit

--- iso1.sql
+++ iso1.sql
@@ -7,16 +7,16 @@ SET SESSION TRANSACTION ISOLATION LEVEL READ COMMITTED;
 START TRANSACTION;
 
 -- Display initial product data
-SELECT product_id, product_name, stock_quantity FROM products WHERE product_id = 1;
+SELECT product_id, product_name, stock_quantity FROM products WHERE product_id = 2;
 
 -- Update the product stock
-UPDATE products SET stock_quantity = stock_quantity - 5 WHERE product_id = 1;
+UPDATE products SET stock_quantity = stock_quantity - 5 WHERE product_id = 2;
 
 -- Check the updated product data (visible to this transaction)
-SELECT product_id, product_name, stock_quantity FROM products WHERE product_id = 1;
+SELECT product_id, product_name, stock_quantity FROM products WHERE product_id = 2;
 
 -- This will make the script pause for 30 seconds to give you time to run iso2.sql
-SELECT SLEEP(30) AS 'Waiting - Run iso2.sql now in another connection window';
+SELECT SLEEP(20) AS 'Waiting - Run iso2.sql now in another connection window';
 
 -- Now manually commit
 COMMIT;
--- iso2.sql
+++ iso2.sql
@@ -5,12 +5,12 @@ SET SESSION TRANSACTION ISOLATION LEVEL READ COMMITTED;
 START TRANSACTION;
 
 -- Check product data (depending on isolation level, may or may not see connection 1's updates)
-SELECT product_id, product_name, stock_quantity FROM products WHERE product_id = 1;
+SELECT product_id, product_name, stock_quantity FROM products WHERE product_id = 2;
 
 -- Try to update the same row (may block depending on isolation level)
-UPDATE products SET stock_quantity = stock_quantity - 2 WHERE product_id = 1;
+UPDATE products SET stock_quantity = stock_quantity - 2 WHERE product_id = 2;
 
 -- Check data after update
-SELECT product_id, product_name, stock_quantity FROM products WHERE product_id = 1;
+SELECT product_id, product_name, stock_quantity FROM products WHERE product_id = 2;
 
 COMMIT;
